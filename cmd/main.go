5a3271e8c14263e137aafd15d3b059d0d087f42d
EldenaScroll <2759359719sw@gmail.com>
2026-01-03 20:38:40 -0500

    Updated scoring

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,12 @@
 package main
 
-import ("fmt";"log";"time"; "os";"bufio";
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
 	"github.com/EldenaScroll/text-search-engine/pkg/crawler"
 	"github.com/EldenaScroll/text-search-engine/pkg/index"
 	"github.com/EldenaScroll/text-search-engine/pkg/tokenizer"
@@ -12,41 +18,51 @@ func main() {
 	start := time.Now()
 
 	stopWords, err := tokenizer.LoadStopWords(stopWordsPath)
-	if err != nil {log.Fatal(err)}
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	files, filenames, err := crawler.LoadDocuments(dataPath)
 
-	if err != nil {log.Fatal(err)}
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("Loaded %d documents in %v\n", len(files), time.Since(start))
-	
+
 	idx := index.NewIndex()
 
 	if len(files) > 0 {
 
-		for docID, content := range files{
+		for docID, content := range files {
 			tokens := tokenizer.Tokenize(content)
 
 			var cleanTokens []string
-			for _, t := range tokens{
-				if _, isStopWord := stopWords[t]; !isStopWord{
+			for _, t := range tokens {
+				if _, isStopWord := stopWords[t]; !isStopWord {
 					cleanTokens = append(cleanTokens, t)
 				}
 
 			}
-			fmt.Print(cleanTokens,"\n")
+			fmt.Print(cleanTokens, "\n")
 			idx.Add(docID, cleanTokens)
-			fmt.Print(idx,"\n")
+			fmt.Print(idx, "\n")
 		}
 		fmt.Printf("Total setup time: %v\n", time.Since(start))
 		scanner := bufio.NewScanner(os.Stdin)
 
 		for {
 			fmt.Print("\nSearch (or exit to quit)-> ")
-			if !scanner.Scan(){break}
+			if !scanner.Scan() {
+				break
+			}
 			query := scanner.Text()
-			if query == "exit" {break}
+			if query == "exit" {
+				break
+			}
 			queryTokens := tokenizer.Tokenize(query)
-			if len(queryTokens) == 0 {continue}
+			if len(queryTokens) == 0 {
+				continue
+			}
 
 			var searchTokens []string
 
@@ -56,16 +72,20 @@ func main() {
 				}
 			}
 			matchedIDs := idx.Search(searchTokens)
-			if len(matchedIDs) == 0 {fmt.Print("Not Found") }
+			if len(matchedIDs) == 0 {
+				fmt.Print("Not Found")
+			}
 
-			for _, id := range matchedIDs{
-				if id < len(filenames){
-					fmt.Printf("- %s\n", filenames[id])
+			for i, result := range matchedIDs {
+				if i >= 10 {
+					break
+				}
+				if result.DocID < len(filenames) {
+					fmt.Printf("%d. %s (Score: %.2f)\n", i+1, filenames[result.DocID], result.Score)
 				}
 			}
 
 		}
 	}
-	
-	
+
 }
--- pkg/index/index.go
+++ pkg/index/index.go
@@ -1,33 +1,64 @@
 package index
 
-type Index struct{ store map[string][]int }
+import (
+	"math"
+	"sort"
+)
 
-// return an empty Index
-func NewIndex() *Index { return &Index{store: make(map[string][]int)} }
+type Posting struct {
+	DocID int
+	Count int
+}
+
+type Index struct {
+	store     map[string][]Posting
+	totalDocs int
+}
+
+type SearchResult struct {
+	DocID int
+	Score float64
+}
+
+func NewIndex() *Index { return &Index{store: make(map[string][]Posting), totalDocs: 0} }
 
 // add indexes a document
 func (idx *Index) Add(docID int, tokens []string) {
+	idx.totalDocs++
+
+	// map word to frequency
+	termFreq := make(map[string]int)
+
 	for _, token := range tokens {
-		ids := idx.store[token]
-		if len(ids) > 0 && ids[len(ids)-1] == docID {
-			continue
-		}
-		idx.store[token] = append(ids, docID)
+		termFreq[token]++
+	}
+
+	for token, count := range termFreq {
+		posting := Posting{DocID: docID, Count: count}
+		idx.store[token] = append(idx.store[token], posting)
 	}
 
 }
 
-// Search takes a list of query tokens and returns IDs that contain all tokens.
-func (idx *Index) Search(tokens []string) []int {
+// Search takes a list of query tokens and returns result
+func (idx *Index) Search(tokens []string) []SearchResult {
 	// edge case
 	if len(tokens) == 0 {
 		return nil
 	}
 
 	firstWord := tokens[0]
-	finalIDs, ok := idx.store[firstWord]
+	postings, ok := idx.store[firstWord]
 	if !ok {
 		return nil
+	}
+
+	docScores := make(map[int]float64)
+	//initialization
+	idf := math.Log(float64(idx.totalDocs) / float64(len(postings)))
+
+	for _, p := range postings {
+		docScores[p.DocID] = float64(p.Count) * idf
 	}
 
 	// check the rest of the words
@@ -37,17 +68,35 @@ func (idx *Index) Search(tokens []string) []int {
 		if !ok {
 			return nil
 		}
-		
-		// Intersect current results with this new list
-		finalIDs = intersection(finalIDs, nextIDs)
-		
+
+		nextDocScores := make(map[int]float64)
+
+		for _, p := range nextIDs {
+
+			if currentScore, exists := docScores[p.DocID]; exists {
+				nextDocScores[p.DocID] = currentScore + (float64(p.Count) * idf)
+			}
+		}
+
+		docScores = nextDocScores
+
 		// If result becomes empty, stop early
-		if len(finalIDs) == 0 {
+		if len(docScores) == 0 {
 			return nil
 		}
 	}
 
-	return finalIDs
+	var results []SearchResult
+	for docID, score := range docScores {
+		results = append(results, SearchResult{DocID: docID, Score: score})
+	}
+
+	// sort by Score (High to Low)
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Score > results[j].Score
+	})
+
+	return results
 }
 
 func intersection(a, b []int) []int {
@@ -55,10 +104,10 @@ func intersection(a, b []int) []int {
 	if len(b) > maxLen {
 		maxLen = len(b)
 	}
-	
+
 	// avoid resizing overhead
 	r := make([]int, 0, maxLen)
-	
+
 	var i, j int
 	for i < len(a) && j < len(b) {
 		if a[i] < b[j] {
